c35370addbe1dffb451096e9fabf0d941adc9e2a
mazenaliii <125923701+mazenaliii@users.noreply.github.com>
2024-04-11 07:59:23 +0200

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,4 +1,4 @@
 const cors = require('cors')
 require('dotenv').config()
 
-module.exports = cors({ origin: process.env.CLIENT_ORIGIN || "http://localhost:3000" || "http://localhost:3001", 'Access-Control-Allow-Credentials': true });
+module.exports = cors({ origin: process.env.CLIENT_ORIGIN || "http://localhost:3000" || "http://localhost:3001" || "https://reciperushadv.vercel.app" || "https://reciperushadv.onrender.com", 'Access-Control-Allow-Credentials': true });
